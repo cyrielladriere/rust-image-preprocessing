a48e97b78294acef2cdaa9a00c48b0638fad589c
cyrielladriere <cyriel.ladriere@gmail.com>
2024-07-28 12:22:01 +0200

    add new gamma transformations

--- src/image_transformation.rs
+++ src/image_transformation.rs
@@ -14,3 +14,34 @@ pub fn get_negative(img: &DynamicImage) -> RgbaImage {
     }
     out_img
 }
+
+pub fn get_gamma_log_transform(img: &DynamicImage) -> RgbaImage {
+    let (width, height) = img.dimensions();
+    let mut out_img = RgbaImage::new(width, height);
+    let c = 255.0 / f64::log(255.0 + 1.0, 10.0);
+    for y in 0..height {
+        for x in 0..width {
+            let mut pix = img.get_pixel(x, y);
+            for i in 0..3 {
+                pix[i] = (c * f64::log(pix[i] as f64 + 1.0, 10.0)) as u8;
+            }
+            out_img.put_pixel(x, y, pix);
+        }
+    }
+    out_img
+}
+
+pub fn get_gamma_powlaw_transform(img: &DynamicImage, gamma: f64) -> RgbaImage {
+    let (width, height) = img.dimensions();
+    let mut out_img = RgbaImage::new(width, height);
+    for y in 0..height {
+        for x in 0..width {
+            let mut pix = img.get_pixel(x, y);
+            for i in 0..3 {
+                pix[i] = (255.0 * f64::powf(pix[i] as f64 / 255.0, 1.0 / gamma)) as u8;
+            }
+            out_img.put_pixel(x, y, pix);
+        }
+    }
+    out_img
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod utils;
 use std::path::Path;
 
 use crate::image_info::{average_brightness, calc_blur};
-use crate::image_transformation::get_negative;
+use crate::image_transformation::{
+    get_gamma_log_transform, get_gamma_powlaw_transform, get_negative,
+};
 use image::{DynamicImage, GenericImageView};
 
 fn main() {
@@ -25,4 +27,14 @@ fn main() {
     let path = Path::new("images/negative_image.png");
     negative_img.save(path).unwrap();
     println!("negative image saved at: {:?}", path);
+
+    let gamma_log = get_gamma_log_transform(&img);
+    let path = Path::new("images/gamma_log_transform.png");
+    gamma_log.save(path).unwrap();
+    println!("gamma log transform saved at: {:?}", path);
+
+    let gamma_powlaw = get_gamma_powlaw_transform(&img, 0.7);
+    let path = Path::new("images/gamma_powlaw_transform.png");
+    gamma_powlaw.save(path).unwrap();
+    println!("gamma powlaw transform saved at: {:?}", path);
 }
